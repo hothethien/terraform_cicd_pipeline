cc5ef834164c0b0cdc9e38fa70420a1503eff5f8
Thien Ho <thienht@vnpt.vn>
2022-07-16 22:33:15 +0700

    changes

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
     organization = "job_16072022"
 
     workspaces {
-      name = "remote_backend"
+      name = "terraform_cicd_pipeline"
     }
   }
 }   
@@ -16,19 +16,19 @@ locals {
   project = "terraform-series"
 }
 
-data "aws_ami" "ami" {
-  most_recent = true
+# data "aws_ami" "ami" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
 
-#   owners = ["099720109477"]
-}
+# #   owners = ["099720109477"]
+# }
 
 resource "aws_instance" "ansible_server" {
-  ami           = data.aws_ami.ami.id
+  ami           = "ami-02ee763250491e04a"
   instance_type = "t2.micro"
 
   lifecycle {
